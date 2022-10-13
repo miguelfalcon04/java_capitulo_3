dbdfbda62e6e79d551f49361a31fe3be8eef656c
miguelfal <falconmiguel.2004@gmail.com>
2022-10-13 20:23:13 +0200

    commit

---
+++ Ejercicio9.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Ejercicio9 {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduzca la altura (cm): ");
+        double altura = sc.nextDouble();
+        System.out.print("Introduzca el radio de la base (cm): ");
+        double radio = sc.nextDouble();
+        double vol = ((double));
+
+    }
+}
