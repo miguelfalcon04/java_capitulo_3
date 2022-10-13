0afbbe642fad2a5058f90c79ffa085535d1239c6
miguelfal <falconmiguel.2004@gmail.com>
2022-10-13 20:22:55 +0200

    commit

---
+++ Ejercicio5.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Ejercicio5{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduzca altura del rectángulo (cm): ");
+        double h = Double.parseDouble(System.console().readLine());
+        System.out.print("Introduzca la base del rectángulo (cm): ");
+        double b = Double.parseDouble(System.console().readLine());
+        System.out.println("El área del rectangulo es: " + (b*h) + "cm^2");
+        sc.close();
+    }
+
+}
