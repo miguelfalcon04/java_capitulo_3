4f9f1d326268d5943525a3c1d90d83305ed85227
miguelfal <falconmiguel.2004@gmail.com>
2022-10-13 20:58:52 +0200

    commit

---
+++ Ejercicio10.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Ejercicio10 {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduzca los mb para convertir: ");
+        double mb = sc.nextDouble();
+        System.out.println(mb + " son " + (mb*1000) + "kb");
+        sc.close();
+    }
+}
